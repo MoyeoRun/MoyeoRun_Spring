e8af28340fac0888cacc73c9308530786104bbcd
inhyeok <ss112d86@gmail.com>
2022-05-09 21:39:59 +0900

    Test: UserController 단위테스트

--- src/main/java/com/moyeorun/auth/domain/auth/dto/request/NicknameDuplicateRequest.java
+++ src/main/java/com/moyeorun/auth/domain/auth/dto/request/NicknameDuplicateRequest.java
@@ -3,10 +3,12 @@ package com.moyeorun.auth.domain.auth.dto.request;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NicknameDuplicateRequest {
 
   @NotBlank
