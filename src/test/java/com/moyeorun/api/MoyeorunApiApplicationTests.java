0036ef9cb3797c8bcff73d049ee62ea5cbd081ba
inhyeok <ss112d86@gmail.com>
2022-08-11 17:56:35 +0900

    Refactor: application.yml profile 적용 및 docker수정

--- src/test/java/com/moyeorun/api/MoyeorunApiApplicationTests.java
+++ src/test/java/com/moyeorun/api/MoyeorunApiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class AuthApplicationTests {
+class MoyeorunApiApplicationTests {
 
   @Test
   void contextLoads() {
