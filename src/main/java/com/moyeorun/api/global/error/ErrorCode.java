7537b09d69eebdd8d7bd467671ec5f87b67ba2aa
Jo In Hyeok <ss112d86@gmail.com>
2022-09-01 15:54:42 +0900

    Fix: ErrorCode 중복 수정 (#13)
    * Fix: ErrorCode 중복 수정
    
    * Refactor: ErrorCode 도메인 별로 분리
    
    * Fix: 다른방 중복 참여Error 메시지 수정
    이미 참여중인 방입니다 -> 이미 다른방에 참여중입니다.

--- src/main/java/com/moyeorun/api/global/error/ErrorCode.java
+++ src/main/java/com/moyeorun/api/global/error/ErrorCode.java
@@ -12,22 +12,22 @@ public enum ErrorCode {
   ENTITY_NOT_FOUND(103, "해당 자원을 찾을 수 없습니다.", 404),
   EXPIRED_JWT(104, "토큰 기간 만료", 401),
   NOT_SIGN_IN_USER(105, "로그인이 필요합니다.", 401),
-  ALREADY_PARTICIPATE_ROOM(106, "이미 참여된 방입니다.", 400),
-  NOT_ALLOW_RESERVATION_REQUEST(107, "방 예약과 관련된 시간이 아닙니다.", 400),
-  ALREADY_RESERVATION_ROOM(108, "이미 예약한 방입니다.", 400),
-  REQUIRE_RESERVATION_ROOM(109, "예약된 유저가 아닙니다,", 400),
-  NOT_ALLOW_HOST_USER_REQUEST(110, "방장은 해당 요청을 보낼 수 없습니다.", 400),
-  NOT_ALLOW_JOIN_REQUEST(111, "방 참여와 관련된 시간이 아닙니다.", 400),
-  ALREADY_JOIN_ROOM(112, "이미 참여한 방입니다.", 400),
-  REQUIRE_JOIN_ROOM(113, "참여한 유저가 아닙니다.", 400),
-  LIMIT_ROOM_USER_COUNT(114, "방이 최대 인원입니다.", 400),
-  NOT_DELETE_ROOM_TIME(115, "방 삭제를 할 수 없는 시간입니다.", 400),
-  NOT_SUPPORT_METHOD(198, "지원히지 않는 메소드입니다.", 400),
-  INTERNAL_SERVER_ERROR(199, "서버 에러", 500),
 
   NICKNAME_DUPLICATE(110, "닉네임 중복됐습니다.", 400),
-  SNS_USER_DUPLICATE(111, "이미 가입된 유저입니다.", 400);
+  SNS_USER_DUPLICATE(111, "이미 가입된 유저입니다.", 400),
 
+  ALREADY_PARTICIPATE_ROOM(120, "이미 다른 방에 참여중입니다.", 400),
+  NOT_ALLOW_RESERVATION_REQUEST(121, "방 예약과 관련된 시간이 아닙니다.", 400),
+  ALREADY_RESERVATION_ROOM(122, "이미 예약한 방입니다.", 400),
+  REQUIRE_RESERVATION_ROOM(123, "예약된 유저가 아닙니다,", 400),
+  NOT_ALLOW_HOST_USER_REQUEST(124, "방장은 해당 요청을 보낼 수 없습니다.", 400),
+  NOT_ALLOW_JOIN_REQUEST(125, "방 참여와 관련된 시간이 아닙니다.", 400),
+  ALREADY_JOIN_ROOM(126, "이미 참여한 방입니다.", 400),
+  REQUIRE_JOIN_ROOM(127, "참여한 유저가 아닙니다.", 400),
+  LIMIT_ROOM_USER_COUNT(128, "방이 최대 인원입니다.", 400),
+  NOT_DELETE_ROOM_TIME(129, "방 삭제를 할 수 없는 시간입니다.", 400),
+  NOT_SUPPORT_METHOD(198, "지원히지 않는 메소드입니다.", 400),
+  INTERNAL_SERVER_ERROR(199, "서버 에러", 500);
 
   private final int errorCase;
   private final String message;
